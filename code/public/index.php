15459099572807e533cefa34895040fd1fdb23a5
Goodwin <akterrific@gmail.com>
2020-03-09 19:00:26 +0200

    refactoring

--- code/public/index.php
+++ code/public/index.php
@@ -1,17 +1,4 @@
 <?php
 require_once '../vendor/autoload.php';
 
-$url = 'http://news.yahoo.com/rss/';
-$rss = Feed::loadRss($url);
-
-echo 'Title: ', $rss->title;
-echo 'Description: ', $rss->description;
-echo 'Link: ', $rss->link;
-
-foreach ($rss->item as $item) {
-    echo 'Title: ', $item->title;
-    echo 'Link: ', $item->link;
-    echo 'Timestamp: ', $item->timestamp;
-    echo 'Description ', $item->description;
-    echo 'HTML encoded content: ', $item->{'content:encoded'};
-}
+echo phpinfo();
